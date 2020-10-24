938c88c2a891c9285e2f57dbe857d76324739c1e
Winkler Jeffrey <wjeffrey@localhost.localdomain>
2020-10-24 20:11:50 +0200

    modulo functionnal

--- infinop/infinmod.c
+++ infinop/infinmod.c
@@ -13,8 +13,8 @@ char *infinmod(char *nb1, char *nb2)
 {
     char *div = infindiv(nb1, nb2);
     int nb1_len = my_strlen(nb1);
-    char mult;
-    char sub;
+    char *mult;
+    char *sub;
 
     mult = infinmult(div, nb2);
     sub = infinsub(nb1, mult);
