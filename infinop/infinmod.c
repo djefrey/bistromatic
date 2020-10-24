2ea2320bb807b1491ccf2cb672c0504943232b8c
Winkler Jeffrey <wjeffrey@localhost.localdomain>
2020-10-24 19:12:54 +0200

    semi functionnal mod

--- infinop/infinmod.c
+++ infinop/infinmod.c
@@ -13,13 +13,10 @@ char *infinmod(char *nb1, char *nb2)
 {
     char *div = infindiv(nb1, nb2);
     int nb1_len = my_strlen(nb1);
-    char *ten_factor = malloc(sizeof(char) * (nb1_len + 1));
     char mult;
     char sub;
 
-    my_fillstr(ten_factor, '0', nb1_len);
-    ten_factor[0] = '1';
-    mult = infinmult(div, ten_factor);
+    mult = infinmult(div, nb2);
     sub = infinsub(nb1, mult);
     return (sub);
 }
