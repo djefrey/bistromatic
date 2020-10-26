4048378d6f3070eb3555813ad7c740b486e257f9
Winkler Jeffrey <wjeffrey@localhost.localdomain>
2020-10-26 11:45:58 +0100

    add cleanexpr.c

---
+++ cleanexpr.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** bistromatic
+** File description:
+** clean expression
+*/
+
+#include "cleanexpr.h"
+
+char *clean_expr(char *expr, char *base, char *operators)
+{
+    
+}
