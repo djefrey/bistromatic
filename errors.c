4595d620272945112e85facb8b0f858d4c1a8c21
Winkler Jeffrey <wjeffrey@localhost.localdomain>
2020-10-27 10:13:58 +0100

    edit error

--- errors.c
+++ errors.c
@@ -14,14 +14,14 @@ static char valid_base_ops(char *base, char *operators)
     int op_len = my_strlen(operators);
 
     if (base_len < 2 || op_len != 7)
-        return (0);
+        return (1);
     for (int i = 0; i < base_len; i++) {
         for (int j = 0; j < op_len; j++) {
             if (base[i] == operators[j])
-                return (0);
+                return (1);
         }
     }
-    return (1);
+    return (0);
 }
 
 static char valid_brackets(char *expr, char *operators)
@@ -35,25 +35,14 @@ static char valid_brackets(char *expr, char *operators)
         else if (expr[i] == CLOSE_BRACKET(operators))
             open_brackets--;
     }
-    return (open_brackets == 0);
-}
-
-static char check_div_by_zero(char *expr, char *base, char *operators)
-{
-    int expr_len = my_strlen(expr);
-
-    for (int i = 0; i < expr_len; i++) {
-        if (expr[i] == DIV_OP(operators) && expr[i + 1] == base[0])
-            return(0);
-    }
-    return (1);
+    return (open_brackets != 0);
 }
 
 char check_error(char *expr, char *base, char *operators)
 {
-    if (!valid_base_ops(base, operators) || !valid_brackets(expr, operators))
-        return (1);
-    else if (!check_div_by_zero(expr, base, operators))
-        return (1);
-    return (0);
+    int error = 0;
+
+    error += valid_base_ops(base, operators);
+    error += valid_brackets(expr, operators));
+    return (error > 0);
 }
